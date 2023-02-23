04d659281f013fd3e1603f84e143d889f339cdf0
MiroHann <122731952+MiroHann@users.noreply.github.com>
2023-02-23 17:00:48 +0200

    Delete database.js

--- Viikko 7/Tehtava 16 Koodit/database.js
+++
@@ -1,8 +0,0 @@
-const mysql = require('mysql');
-const connection = mysql.createPool({
-  host: '127.0.0.1',
-  user: 'root',
-  password: 'Porkkana123',
-  database: 'book'
-});
-module.exports = connection;
